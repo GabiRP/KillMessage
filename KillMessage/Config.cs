5465b277bb225139e305c6028cd45ff7df021586
GabiRP <57387907+GabiRP@users.noreply.github.com>
2022-03-08 16:22:35 +0100

    cfg

--- KillMessage/Config.cs
+++ KillMessage/Config.cs
@@ -14,7 +14,7 @@ namespace KillMessage
         [Description("Whether or not use permissions to be able to use the command (permission: kmsg)")]
         public bool UsePermissions { get; set; } = false;
 
-        [Description("List of available colors. MAKE SURE TO WRITE THEM IN LOWER CASE")]
+        [Description("List of available colors. MAKE SURE TO WRITE THEM IN LOWER CASE AND USE SCPSL WIKI COLORS")]
         public string[] AvailableColors { get; set; } = new[]
         {
             "pink", "red", "brown", "silver", "light_green", "crimson", "cyan", "aqua", "deep_pink", "tomato", "yellow", "magenta", "blue_green",
