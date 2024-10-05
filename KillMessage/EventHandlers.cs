9e0f435b0ee9b8da4769e1ba50a0bc0ae98675d9
GabiRP <57387907+GabiRP@users.noreply.github.com>
2024-10-05 15:38:24 +0200

    cleanup + bump

--- KillMessage/EventHandlers.cs
+++ KillMessage/EventHandlers.cs
@@ -8,7 +8,9 @@ namespace KillMessage
     {
         internal void OnDied(DiedEventArgs ev)
         {
-            if(ev.Player.GetDisabled() || ev.Attacker is null || ev.Player is null || string.IsNullOrEmpty(ev.Attacker.GetMessage()) || (!Plugin.Singleton.Config.ShowOnSuicide && ev.Player == ev.Attacker)) return;
+            if(ev.Player.GetDisabled() || ev.Attacker is null || ev.Player is null 
+               || string.IsNullOrEmpty(ev.Attacker.GetMessage()) 
+               || (!Plugin.Singleton.Config.ShowOnSuicide && ev.Player == ev.Attacker)) return;
             
             string message = $"<size={Plugin.Singleton.Config.MessageSize}><color={ev.Attacker.GetColor()}>{Plugin.Singleton.Translation.Message.Replace("$message", ev.Attacker.GetMessage()).Replace("$author", ev.Attacker.Nickname)}</color></size>";
 
@@ -27,7 +29,11 @@ namespace KillMessage
             {
                 string current = ev.Player.GetMessage();
                 string color = ev.Player.GetColor();
-                ev.Player.SendConsoleMessage(Plugin.Singleton.Translation.ConsoleMessage.Replace("$helpmsg", Plugin.Singleton.Translation.HelpMessage).Replace("$current", string.IsNullOrEmpty(current) ? Plugin.Singleton.Translation.MessageNotSet : current).Replace("$color", color),
+                ev.Player.SendConsoleMessage(Plugin.Singleton.Translation.ConsoleMessage
+                        .Replace("$helpmsg", Plugin.Singleton.Translation.HelpMessage)
+                        .Replace("$current", string.IsNullOrEmpty(current) ? Plugin.Singleton.Translation.MessageNotSet 
+                            : current)
+                        .Replace("$color", color),
                     "default");
             }
         }
